ed3a4434c83c1d68ea2a122b197a08a37a98d7c9
Leo Ly <41182741+LeoLy77@users.noreply.github.com>
2022-11-28 13:25:08 +1000

    Add files via upload

---
+++ arduino.ino
@@ -0,0 +1,81 @@
+#include <FastLED.h>
+#include <string.h>
+#define LED_PIN     3
+#define NUM_LEDS    16
+#define BRIGHTNESS  70
+CRGB leds[NUM_LEDS];
+//uint32_t inputSerial = 0;         // a String to hold incoming data
+uint32_t inputSerial[NUM_LEDS] = {0x832090, 0x862195, 0x862196, 
+  0x852297, 0x86239A, 0x87249C, 
+  0x87249D, 0x86259F, 0x8726A2, 
+  0x8725A3, 0x8725A3, 0x8926A4, 
+  0x8926A4, 0x8926A6, 0x8926A6, 0x8926A6}; 
+
+uint8_t r,g,b;
+uint8_t led_i = 0;
+bool stringComplete = false;  // whether the string is complete
+int8_t shiftBits = 0;
+uint8_t recvCnt = 0;
+// the setup routine runs once when you press reset:
+void setup() {
+//  FastLED.addLeds<WS2812B, LED_PIN, GBR >(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, LED_PIN, RBG >(leds, NUM_LEDS);
+  FastLED.setBrightness( BRIGHTNESS );
+  Serial.begin(115200);
+  memset(inputSerial, 0, sizeof(uint32_t)*NUM_LEDS);
+}
+
+
+void serialEvent() {
+  uint8_t inChar;
+  while (Serial.available()) {
+    inChar = Serial.read();
+
+      if (stringComplete == false) {
+  
+        inputSerial[recvCnt] |= ((inChar & 0xFF) << 8*shiftBits);
+        shiftBits++;
+        if (shiftBits == 3) {
+          shiftBits = 0;
+          recvCnt++;
+         
+          if (recvCnt == NUM_LEDS) {
+            stringComplete = true;
+            recvCnt = 0;
+          } 
+        }
+
+    }
+  }
+}
+
+void loop() {
+  if (stringComplete) {
+    int brightness = 0;
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB(inputSerial[NUM_LEDS - 1 - i]);   
+//      brightness += inputSerial[NUM_LEDS - 1 - i];
+    }
+//    brightness = brightness/3;
+//    //normalise
+//    brightness = (brightness - 20)/(90 - 20);
+//    FastLED.setBrightness(brightness);
+    FastLED.show();
+
+    memset(inputSerial, 0, sizeof(uint32_t)*NUM_LEDS);
+    stringComplete = false;
+  }
+}
+
+//void serialEvent() {
+//  while (Serial.available()) {
+//    uint8_t inChar = (uint8_t) Serial.read();
+//    
+//    inputSerial[shiftBits] = inChar;
+//    shiftBits++;
+//    if (shiftBits == NUM_LEDS) {
+//      stringComplete = true;
+//      shiftBits = 0;
+//    }
+//  }
+//}
---
+++ win_pixel_scraper.c
@@ -0,0 +1,158 @@
+/*H**********************************************************************
+* FILENAME :        main.c            
+* DESCRIPTION :
+*       For Windows only
+*       This program sample pixels on Windows (from 10) screen, and sends the data out to serial comm
+* AUTHOR :    Leo Ly        START DATE :    
+*
+*H*/
+
+//gcc main_onlyHor.c -o out -lgdi32
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <windows.h>
+#include <wingdi.h>
+
+#define FALSE 0
+#define TRUE 1
+
+volatile int STOP=FALSE;
+
+int main(void) {
+    //Serial prep
+    HANDLE hComm;  // Handle to the Serial port
+    DCB dcbSerialParams = { 0 };  // Initializing DCB structure
+    DWORD BytesWritten = 1;          // No of bytes written to the port
+    DWORD dwEventMask;     // Event mask to trigger
+    char  ReadData;        //temperory Character
+    DWORD NoBytesRead;     // Bytes read by ReadFile()
+    unsigned char loop = 0;
+
+    //Open the serial com port
+    hComm = CreateFile("COM8", 
+                       GENERIC_WRITE,      // Read/Write Access
+                       0,                                 // No Sharing, ports cant be shared
+                       NULL,                              // No Security
+                       OPEN_EXISTING,                     // Open existing port only
+                       0,                                 // Non Overlapped I/O
+                       NULL);                             // Null for Comm Devices
+    if (hComm == INVALID_HANDLE_VALUE) {
+        printf("\n Port can't be opened\n\n");
+    }
+    //Setting the Parameters for the SerialPort
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    int Status = GetCommState(hComm, &dcbSerialParams); //retreives  the current settings
+    if (Status == FALSE) {
+        printf("\nError to Get the Com state\n\n");
+    }
+
+    dcbSerialParams.BaudRate = CBR_115200;      
+    dcbSerialParams.ByteSize = 8;            
+    dcbSerialParams.StopBits = ONESTOPBIT;    
+    dcbSerialParams.Parity = NOPARITY;      
+    Status = SetCommState(hComm, &dcbSerialParams);
+    if (Status == FALSE) {
+        printf("\nError to Setting DCB Structure\n\n");
+    }
+
+    //Setting Timeouts
+    COMMTIMEOUTS timeouts = { 0 };  //Initializing timeouts structure
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 50;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+    timeouts.WriteTotalTimeoutConstant = 50;
+    timeouts.WriteTotalTimeoutMultiplier = 10;
+    if (SetCommTimeouts(hComm, &timeouts) == FALSE) {
+        printf("\nError to Setting Time outs");
+    }
+
+
+    //Screen scraping prep
+    int scn_height = GetSystemMetrics(SM_CYSCREEN);
+    int scn_width = GetSystemMetrics(SM_CXSCREEN);
+    // printf("scn_height=%d scn_width=%d\n", scn_height, scn_width);
+    HWND desktop =  GetDesktopWindow();
+    HDC desktopHdc = GetDC(desktop);
+
+    int cnt = 0, x, y = 30; 
+    int no_Yportions = 3;
+    int y_arr[no_Yportions];
+    for (y = 1; y < scn_height; y+=scn_height/no_Yportions) {
+        y_arr[cnt] = y;
+        cnt += 1;
+    }
+    int y_centre = scn_height/2;
+
+    cnt = 0;
+    int no_Xportions = 16;
+    int x_arr[no_Xportions];
+    for (x = 1; x < scn_width; x+=scn_width/no_Xportions) {
+        x_arr[cnt] = x;
+        cnt += 1;
+    }
+
+    // uint8_t ret_size = no_Xportions*2+no_Yportions*2-4;
+    // COLORREF ret_arr[ret_size];
+
+    COLORREF ret_arr[no_Xportions];
+    COLORREF colour, colour_centre, colour_above, colour_below;
+    uint8_t ret_arr_cnt = 0;
+
+    while (1) {
+
+        for (int i = 0; i < no_Xportions; i++) {
+            colour_centre = GetPixel(desktopHdc, x_arr[i], y_centre);//y_arr[i]);
+            colour_above = GetPixel(desktopHdc, x_arr[i], y_centre+80);
+            colour_below = GetPixel(desktopHdc, x_arr[i], y_centre-80);
+
+            //average hex colour
+            for (int k = 0; k < 3; k++) {
+                int a = ((colour_above >> 8*k) & 0xFF);
+                int b = ((colour_centre >> 8*k) & 0xFF);
+                int c = ((colour_below >> 8*k) & 0xFF);
+                int sum = a + c;
+                int res = sum/3;
+
+                colour |= ((res & 0xFF) << 8*k);
+            }
+
+            ret_arr[i] = colour;
+        }
+        for (int i = 0; i < no_Xportions; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                uint8_t buf = ((ret_arr[i] >> j*8) & 0xFF);
+                WriteFile(hComm, (void*) &buf, sizeof(uint8_t), &BytesWritten, NULL);
+            }
+        }
+    }
+
+    return 0;
+}
+
+    // while (1) {
+    //         for (int i = 0; i < no_Xportions; i++) {
+    //             colour = GetPixel(desktopHdc, x_arr[i], y_centre);//y_arr[i]);
+    //             ret_arr[i] = colour;
+    //             // ret_arr_cnt++;
+
+    //             for (int j = 0; j < 3; j++) {
+    //                 buf[0] = ((ret_arr[j] >> j*8) & 0xFF);
+    //                 WriteFile(hComm, (void*) buf, sizeof(uint8_t), &BytesWritten, NULL);
+    //             }
+    //         }
+    //         // ret_arr_cnt = 0;
+
+
+    //         // for (int i = 0; i < no_Xportions; i++) {
+    //             // printf("%06X \n\r", ret_arr[i]);
+    //             // for (int j = 0; j < 3; j++) {
+    //             //     buf[0] = ((ret_arr[i] >> j*8) & 0xFF);
+    //             //     WriteFile(hComm, (void*) buf, sizeof(uint8_t), &BytesWritten, NULL);
+    //             // }
+    //             // Sleep(1);
+    //         // }
+    //     // for (int i = 0; i < no_Yportions; i++) {
+    //     // }
+    // }
